2cd991a377e9b401113d1d953a5d774a652e75bd
Aleksander <sanya.slepow2015@yandex.ru>
2024-08-21 12:42:10 +0500

    homework15.1 1

--- src/category.py
+++ src/category.py
@@ -21,6 +21,9 @@ class Category:
         Category.product_count += len(products)
         print(Category.product_count)
 
+    def __str__(self):
+        return f"{self.name}, количество продуктов: {len(self.__products)} шт."
+
     def add_product(self, product: Product) -> Any:
         self.__products.append(product)
         Category.product_count += 1
@@ -29,8 +32,15 @@ class Category:
     def get_product_list(self) -> str:
         product_list = ""
         for product in self.__products:
-            product_list += f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт.\n"
+            product_list += f"{str(product)}\n"
         return product_list
+
+    @property
+    def products(self) -> list:
+        products_list = []
+        for product in self.__products:
+            products_list.append(product)
+        return products_list
 
 
 # result = Category("Product", "Description", ["product1", "product2", "product3"])
